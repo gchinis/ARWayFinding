b46a4275fdffc0af80657842c302a2abce7368af
Lisa Junger <ljunger@thoughtworks.com>
2016-09-21 09:58:38 +0200

    starting test suite .

--- spec/unit/testSpec.js
+++ spec/unit/testSpec.js
@@ -1,5 +1,19 @@
-describe("A suite", () => {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+var request = require("request");
+var base_url = "http://localhost:8080/"
+
+// describe("A suite", () => {
+//   it("contains spec with an expectation", function() {
+//     expect(true).toBe(false);
+//   });
+// });
+
+describe("the server is started", ()=>{
+  describe("GET /", ()=>{
+    it("returns status code 200", (done)=>{
+      request.get(base_url, function(error, response, body){
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
   });
 });
