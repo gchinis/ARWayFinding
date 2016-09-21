f5ec2fbb52ca0fc2c2eb09a76682eb8d8d940fef
msoto <msoto@eumsoto-3.local>
2016-09-21 17:50:16 +0200

    Working test for marker recognition.
    Yay! The right marker is found on the image.

--- spec/browser/testSpec.js
+++ spec/browser/testSpec.js
@@ -1,11 +1,45 @@
+import "babel-polyfill";
+
+import { artoolkit, ARController, ARCameraParam } from "../../app/artoolkit.js";
 
 describe("markers", () => {
-  it("recognizes a given marker", function() {
+  // Wait for an event to fire once on the given object.
+  const waitForEvent = (eventTarget, eventType) => {
+    return new Promise((resolve, reject) => {
+      let listener = (...args) => {
+        eventTarget.removeEventListener(eventType, listener);
+        resolve(...args);
+      };
+      eventTarget.addEventListener(eventType, listener);
+    });
+  };
+
+  const loadCameraParam = (url) => {
+    return new Promise((resolve, reject) => {
+      let param = new ARCameraParam(url, () => {
+        resolve(param);
+      });
+    });
+  };
+
+  it("recognizes a given marker", (done) => {
+    const marker = '/base/spec/testAssets/marker1.jpg';
     document.body.insertAdjacentHTML(
       'afterbegin',
-      '<img id="marker" src="/base/spec/testAssets/marker1.jpg"></img>'
+      '<img width="640" height="480" id="marker" src="/base/spec/testAssets/marker1.jpg"></img>'
     );
-    const marker = '/base/spec/testAssets/marker1.jpg';
-    expect(document.getElementById('marker')).not.toBe(undefined);
+
+    let imageElem = document.getElementById('marker');
+    expect(imageElem).not.toBe(undefined);
+    waitForEvent(imageElem, 'load').then(() => {
+      return loadCameraParam('/base/spec/testAssets/camera_para.dat');
+    }).then((cameraParam) => {
+      let controller = new ARController(640, 480, cameraParam);
+      controller.setPatternDetectionMode(artoolkit.AR_MATRIX_CODE_DETECTION);
+      controller.detectMarker(imageElem);
+      expect(controller.getMarkerNum()).toBe(1);
+      expect(controller.getMarker(0).id).toBe(20);
+      done();
+    });
   });
 });
