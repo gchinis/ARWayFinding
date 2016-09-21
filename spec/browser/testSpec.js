79b9ee0da76d8651b8b2b0e55db14b26b74e40a8
Lisa Junger <ljunger@thoughtworks.com>
2016-09-21 13:07:31 +0200

    making sure browser tests are running .

--- spec/browser/testSpec.js
+++ spec/browser/testSpec.js
@@ -1,5 +1,11 @@
-describe("A suite", () => {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+
+describe("markers", () => {
+  it("recognizes a given marker", function() {
+    document.body.insertAdjacentHTML(
+      'afterbegin',
+      '<img id="marker" src="/base/spec/testAssets/marker1.jpg"></img>'
+    );
+    const marker = '/base/spec/testAssets/marker1.jpg';
+    expect(document.getElementById('marker')).not.toBe(undefined);
   });
 });
