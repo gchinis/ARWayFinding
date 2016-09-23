de240a13e5559a7c164c749cfd15bf18cf0937a2
Martin Soto <donsoto@gmail.com>
2016-09-23 13:24:16 +0200

    Prevent artoolkit modules from being loaded more than once.
    Otherwise, horrible things happen...

--- app/artoolkit.js
+++ app/artoolkit.js
@@ -4,20 +4,22 @@
 // their public symbols as regular module exports. It requieres quite
 // a bit of Webpack hackery to work at all.
 
-var THREE = require('three');
+if (!window.artoolkit) {
+  var THREE = require('three');
 
-// The main module is translated from C using emscripten and has to be
-// included in pristine form to work at all, hence the "script"
-// loader.
-require("script!./3dparty/jsartoolkit5/build/artoolkit.debug.js");
+  // The main module is translated from C using emscripten and has to be
+  // included in pristine form to work at all, hence the "script"
+  // loader.
+  require("script!./3dparty/jsartoolkit5/build/artoolkit.debug.js");
 
-require("./3dparty/jsartoolkit5/js/artoolkit.api.js");
+  require("./3dparty/jsartoolkit5/js/artoolkit.api.js");
 
-// In order for this module to run its initialization function, THREE
-// must be avaiable globally.
-window.THREE = THREE;
-require("imports?THREE=three&artoolkit=>window.artoolkit&ARController=>window.ARController&ARCameraParam=>window.ARCameraParam!./3dparty/jsartoolkit5/js/artoolkit.three.js");
-delete window.THREE;
+  // In order for this module to run its initialization function, THREE
+  // must be avaiable globally.
+  window.THREE = THREE;
+  require("imports?THREE=three&artoolkit=>window.artoolkit&ARController=>window.ARController&ARCameraParam=>window.ARCameraParam!./3dparty/jsartoolkit5/js/artoolkit.three.js");
+  delete window.THREE;
+}
 
 // Retrieve the exported variables from the window object.
 var [artoolkit, ARController, ARCameraParam] = [
@@ -32,7 +34,7 @@ var [artoolkit, ARController, ARCameraParam] = [
 // referenced by functions in the main artoolkit file although it's
 // only defined in the api file.
 // delete window.artoolkit;
-delete window.ARController;
-delete window.ARCameraParam;
+//delete window.ARController;
+//delete window.ARCameraParam;
 
 export { artoolkit, ARController, ARCameraParam };
