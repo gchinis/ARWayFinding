907de6fc9030c4b3ba857ce6522410acd2473895
Martin Soto <donsoto@gmail.com>
2016-09-26 16:29:33 +0200

    Fold in code from "Three.js from scratch" example.
    This is one of the examples from jsartoolkit5. Also use some of the code
    an algorithms from artoolkit.three.js to make the example run smoothly.

--- app/main.js
+++ app/main.js
@@ -1,7 +1,164 @@
+import "webrtc-adapter";
 const THREE = require('three');
-import { artoolkit, ARController } from "./artoolkit.js";
+import { artoolkit, ARController, ARCameraParam } from "./artoolkit.js";
 
-const main = () => {
+const createAxes = (parentObject) => {
+  var coneX = new THREE.Mesh(
+    new THREE.ConeGeometry(0.1, 1),
+	  new THREE.MeshLambertMaterial({
+      color: 0xff0000,
+      wireframe: false
+    })
+  );
+  coneX.rotateZ(Math.PI / 2);
+  coneX.translateY(0.5);
+
+  var coneY = new THREE.Mesh(
+	  //new THREE.BoxGeometry(1,1,1),
+    new THREE.ConeGeometry(0.1, 1),
+	  new THREE.MeshLambertMaterial({
+      color: 0x00ff00,
+      wireframe: false
+    })
+  );
+  coneY.translateY(0.5);
+
+  var coneZ = new THREE.Mesh(
+    new THREE.ConeGeometry(0.1, 1),
+	  new THREE.MeshLambertMaterial({
+      color: 0x0000ff,
+      wireframe: false
+    })
+  );
+  coneZ.rotateX(Math.PI / 2);
+  coneZ.translateY(0.5);
+
+  parentObject.add(coneX);
+  parentObject.add(coneY);
+  parentObject.add(coneZ);
+};
+
+const cameraLocationInScene = () => {
+  var cMat = new THREE.Matrix4();
+  var tMat = new THREE.Matrix4();
+
+  var renderer = new THREE.WebGLRenderer();
+  var scene = new THREE.Scene();
+
+  renderer.setSize(v.width, v.height);
+
+  document.body.appendChild(renderer.domElement);
+
+  // Create a camera and a marker root object for your Three.js scene.
+  var camera = new THREE.Camera();
+  scene.add(camera);
+
+  var light = new THREE.PointLight(0xffffff);
+  light.position.set(400, 500, 100);
+  scene.add(light);
+  light = new THREE.PointLight(0xffffff);
+  light.position.set(-400, -500, -100);
+  scene.add(light);
+
+  var markerRoot = new THREE.Object3D();
+  markerRoot.matrixAutoUpdate = false;
+
+  markerRoot.wasVisible = false;
+  markerRoot.markerMatrix = new Float64Array(12);
+  markerRoot.matrixAutoUpdate = false;
+  camera.matrixAutoUpdate = false;
+
+  createAxes(markerRoot);
+
+  // Add the marker root to your scene.
+  scene.add(markerRoot);
+  var arController = null;
+
+  var video = document.getElementById('v');
+  navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: 'environment'
+    }
+  }).then((stream) => {
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+  });
+
+  var artoolkitTransform = new Float32Array(12);
+  var glTransform = new Float32Array(16);
+
+  // On every frame do the following:
+  function tick() {
+	  requestAnimationFrame(tick);
+
+	  if (!arController) {
+		  return;
+	  }
+
+	  arController.detectMarker(video);
+	  var markerNum = arController.getMarkerNum();
+    var markerInfo;
+    for (var i = 0; i < markerNum; i++) {
+			markerInfo = arController.getMarker(i);
+      if (markerInfo.id === 2) {
+        break;
+      }
+    }
+    if (i === markerNum) {
+      markerInfo = null;
+    }
+
+	  if (markerInfo) {
+			if (markerInfo.dir !== markerInfo.dirPatt) {
+			  arController.setMarkerInfoDir(i, markerInfo.dirMatrix);
+      }
+
+		  if (markerRoot.visible) {
+			  arController.getTransMatSquareCont(
+          i,
+          1,
+          artoolkitTransform,
+          artoolkitTransform);
+		  } else {
+			  arController.getTransMatSquare(
+          i /* Marker index */,
+          1 /* Marker width */,
+          artoolkitTransform);
+		  }
+      arController.transMatToGLMat(artoolkitTransform, glTransform);
+      markerRoot.matrix.elements.set(glTransform);
+
+		  markerRoot.visible = true;
+	  } else {
+		  markerRoot.visible = false;
+	  }
+
+	  arController.debugDraw();
+
+	  // Render the scene.
+	  renderer.autoClear = false;
+	  renderer.clear();
+	  renderer.render(scene, camera);
+  }
+
+  tick();
+
+  var cameraParam = new ARCameraParam();
+  cameraParam.onload = function() {
+	  arController = new ARController(320, 240, cameraParam);
+    arController.setPatternDetectionMode(artoolkit.AR_MATRIX_CODE_DETECTION);
+	  arController.debugSetup();
+
+	  var camera_mat = arController.getCameraMatrix();
+
+	  camera.projectionMatrix.elements.set(camera_mat);
+
+  };
+  cameraParam.load('camera_para.dat');
+};
+
+
+const objectOnMarker = () => {
 	ARController.getUserMediaThreeScene({
     maxARVideoSize: 320,
     cameraParam: '3dparty/jsartoolkit5/Data/camera_para-iPhone 5 rear 640x480 1.0m.dat',
@@ -27,16 +184,25 @@ const main = () => {
 
       document.body.insertBefore(renderer.domElement, document.body.firstChild);
 
-      // See /doc/patterns/Matrix code 3x3 (72dpi)/20.png
-      var markerRoot = arController.createThreeBarcodeMarker(4);
+      var light = new THREE.PointLight(0xffffff);
+      light.position.set(400, 500, 100);
+      arScene.scene.add(light);
+      light = new THREE.PointLight(0xffffff);
+      light.position.set(-400, -500, -100);
+      arScene.scene.add(light);
 
-      var sphere = new THREE.Mesh(
-        new THREE.SphereGeometry(0.5, 8, 8),
-        new THREE.MeshNormalMaterial()
-      );
-      sphere.material.shading = THREE.FlatShading;
-      sphere.position.z = 0.5;
-      markerRoot.add(sphere);
+      // See /doc/patterns/Matrix code 3x3 (72dpi)/20.png
+      var markerRoot = arController.createThreeBarcodeMarker(2);
+
+      // var sphere = new THREE.Mesh(
+      //   new THREE.SphereGeometry(0.5, 8, 8),
+      //   new THREE.MeshNormalMaterial()
+      // );
+      // sphere.material.shading = THREE.FlatShading;
+      // sphere.position.z = 0.5;
+      // markerRoot.add(sphere);
+
+      createAxes(markerRoot);
       arScene.scene.add(markerRoot);
 
       var rotationV = 0;
@@ -50,8 +216,8 @@ const main = () => {
       var tick = function() {
         arScene.process();
         arScene.renderOn(renderer);
-        rotationV += (rotationTarget - sphere.rotation.z) * 0.05;
-        sphere.rotation.z += rotationV;
+        //rotationV += (rotationTarget - sphere.rotation.z) * 0.05;
+        //sphere.rotation.z += rotationV;
         rotationV *= 0.8;
 
         requestAnimationFrame(tick);
@@ -62,4 +228,6 @@ const main = () => {
   });
 };
 
-main();
+
+//objectOnMarker();
+cameraLocationInScene();
