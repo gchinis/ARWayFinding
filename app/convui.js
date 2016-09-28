39540468d640620a61554bbc79070203299d7f52
kmok <kmok@thoughtworks.com>
2016-09-28 16:15:45 +0200

    km: added React code for consecutively appearing chats

--- app/convui.js
+++ app/convui.js
@@ -3,14 +3,57 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-const initConvUI = () => {
-  ReactDOM.render(
-    <h1>Hello, I'm the robot!</h1>,
-    document.getElementById('example')
-  );
-};
+var Parent = React.createClass({
+    render : function () {
+    var robb = [{wait: 1000, text: "Hello, I'm Robb. How can I help you today?"}, {wait: 4000, text: "You're looking for wall paint? I know exactly where you can find some!"}, {wait: 5000, text: "Follow me!"}];
+    var arrayLength = robb.length;
+    setTimeout(function() { $("#ti").hide(); }, 5000);
 
+    var convo = [];
+    { for (var i = 0; i < arrayLength; i++) {
+       convo.push(<Child wait={robb[i].wait} text={robb[i].text} />)
+    }}
 
-// TODO: Remove this when we integrate UI and 3D into a single entry point.
-window.initConvUI = initConvUI;
+    convo.push(
+      <li id="ti">
+        <div className="typing-indicator chat-bubble">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </li>
+    )
+    return (
+        <ul className="chat-list">
+          {convo}
+        </ul>
+      )
+    }
+});
 
+var Child = React.createClass({
+    getInitialState : function () {
+        return({hidden : "hidden"});
+    },
+    componentWillMount : function () {
+        var that = this;
+        setTimeout(function() {
+            that.show();
+        }, that.props.wait);
+    },
+    show : function () {
+        this.setState({hidden : ""});
+    },
+    render : function () {
+        return (
+            <div className={this.state.hidden}>
+                 <li className="chat-bubble">{this.props.text}</li>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <Parent />,
+  document.getElementById('chat-list')
+);
