b67bf7059814accbd17b7063f283809f79d6c13f
Martin Soto <donsoto@gmail.com>
2016-09-29 17:35:43 +0200

    Clean up marker detection code.
    Break down into smaller functions and use generators for better
    organization.

--- app/marker.js
+++ app/marker.js
@@ -25,34 +25,52 @@ const makeMarkerDetector = (cameraParamUrl, markerDefinitions) => {
       markerToDef[m.id] = m;
     }
 
-    return (imageElem) => {
-      controller.detectMarker(imageElem);
-      let detectedMarkers = [];
-      let totalMarkers = controller.getMarkerNum();
-      for (let i = 0; i < totalMarkers; i += 1) {
-        let markerInfo = controller.cloneMarkerInfo(controller.getMarker(i));
+    let artoolkitTransform = new Float32Array(12);
+    let glTransform = new Float32Array(16);
+
+
+    const makeCameraTransform = (markerIndex, markerSize) => {
+      controller.getTransMatSquare(markerIndex, 1, artoolkitTransform);
+      controller.transMatToGLMat(artoolkitTransform, glTransform);
+
+      let markerTransform = new THREE.Matrix4().fromArray(glTransform);
+      let cameraTransform = new THREE.Matrix4().getInverse(markerTransform);
+
+      cameraTransform.premultiply(new THREE.Matrix4().makeScale(
+        markerSize, markerSize, markerSize
+      ));
+
+      return cameraTransform;
+    };
+
+    const getDetectedMarkerInfo = function* () {
+      let markerCount = controller.getMarkerNum();
+      for (let markerIndex = 0; markerIndex < markerCount; markerIndex += 1) {
+        yield [markerIndex, controller.cloneMarkerInfo(controller.getMarker(markerIndex))];
+      }
+    };
+
+    const getDetectedMarkerDefs = function* () {
+      for (let [markerIndex, markerInfo] of getDetectedMarkerInfo()) {
         let markerDef = markerToDef[markerInfo.id];
         if (markerDef !== undefined) {
-          let artoolkitTransform = new Float32Array(12);
-          let glTransform = new Float32Array(16);
-
-          controller.getTransMatSquare(i, 1, artoolkitTransform);
-          controller.transMatToGLMat(artoolkitTransform, glTransform);
-
-          let markerTransform = new THREE.Matrix4().fromArray(glTransform);
-          let cameraTransform = new THREE.Matrix4().getInverse(markerTransform);
-
-          cameraTransform.premultiply(new THREE.Matrix4().makeScale(
-            markerDef.size, markerDef.size, markerDef.size
-          ));
-
-          detectedMarkers.push({
-            id: markerInfo.id,
-            cameraTransform: cameraTransform
-          });
+          yield [markerIndex, markerInfo, markerDef];
         }
       }
-      return detectedMarkers;
+    };
+
+    const makeDetectedMarkerResults = function* () {
+      for (let [markerIndex, markerInfo, markerDef] of getDetectedMarkerDefs()) {
+        yield {
+          id: markerInfo.id,
+          cameraTransform: makeCameraTransform(markerIndex, markerDef.size)
+        };
+      }
+    };
+
+    return (imageElem) => {
+      controller.detectMarker(imageElem);
+      return Array.from(makeDetectedMarkerResults());
     };
   });
 };
