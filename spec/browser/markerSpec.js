15b460e478809dbe1ff14eba57fa2ec4af1b93c3
Martin Soto <donsoto@gmail.com>
2016-09-29 17:22:47 +0200

    Additional camera position test cases.

--- spec/browser/markerSpec.js
+++ spec/browser/markerSpec.js
@@ -78,9 +78,99 @@ describe("Marker recognition", () => {
         expect(markers[0].id).to.equal(2);
 
         var cameraPos = determineCameraPosition(markers[0].cameraTransform);
-        expect(cameraPos.x).to.be.within(-0.2, 0.2);
-        expect(cameraPos.y).to.be.within(-0.2, 0.2);
-        expect(cameraPos.z).to.be.within(0.2, 0.4);
+        expect(cameraPos.x).to.be.within(-0.20, -0.10);
+        expect(cameraPos.y).to.be.within(-0.05, 0.05);
+        expect(cameraPos.z).to.be.within(0.30, 0.40);
+      });
+    });
+  }));
+
+  it("determines right camera position when marker is seen from bottom", testPromise(() => {
+    return makeMarkerDetector(cameraParamUrl, markerDefinitions).then((detectMarkers) => {
+      return loadImage('marker_3x3_seen_from_bottom.jpg').then((image) => {
+        let markers = detectMarkers(image);
+        expect(markers.length).to.equal(1);
+        expect(markers[0].id).to.equal(2);
+
+        var cameraPos = determineCameraPosition(markers[0].cameraTransform);
+        expect(cameraPos.x).to.be.within(-0.05, 0.05);
+        expect(cameraPos.y).to.be.within(0.20, 0.30);
+        expect(cameraPos.z).to.be.within(0.35, 0.45);
+      });
+    });
+  }));
+
+  it("determines right camera position when marker is seen from top", testPromise(() => {
+    return makeMarkerDetector(cameraParamUrl, markerDefinitions).then((detectMarkers) => {
+      return loadImage('marker_3x3_seen_from_top.jpg').then((image) => {
+        let markers = detectMarkers(image);
+        expect(markers.length).to.equal(1);
+        expect(markers[0].id).to.equal(2);
+
+        var cameraPos = determineCameraPosition(markers[0].cameraTransform);
+        expect(cameraPos.x).to.be.within(-0.05, 0.05);
+        expect(cameraPos.y).to.be.within(-0.10, 0.00);
+        expect(cameraPos.z).to.be.within(0.35, 0.45);
+      });
+    });
+  }));
+
+  it("determines right camera position when marker is seen from left", testPromise(() => {
+    return makeMarkerDetector(cameraParamUrl, markerDefinitions).then((detectMarkers) => {
+      return loadImage('marker_3x3_seen_from_left.jpg').then((image) => {
+        let markers = detectMarkers(image);
+        expect(markers.length).to.equal(1);
+        expect(markers[0].id).to.equal(2);
+
+        var cameraPos = determineCameraPosition(markers[0].cameraTransform);
+        expect(cameraPos.x).to.be.within(0.20, 0.30);
+        expect(cameraPos.y).to.be.within(-0.05, 0.05);
+        expect(cameraPos.z).to.be.within(0.25, 0.35);
+      });
+    });
+  }));
+
+  it("determines right camera position when marker is seen from right", testPromise(() => {
+    return makeMarkerDetector(cameraParamUrl, markerDefinitions).then((detectMarkers) => {
+      return loadImage('marker_3x3_seen_from_right.jpg').then((image) => {
+        let markers = detectMarkers(image);
+        expect(markers.length).to.equal(1);
+        expect(markers[0].id).to.equal(2);
+
+        var cameraPos = determineCameraPosition(markers[0].cameraTransform);
+        expect(cameraPos.x).to.be.within(-0.30, -0.20);
+        expect(cameraPos.y).to.be.within(-0.05, 0.05);
+        expect(cameraPos.z).to.be.within(0.25, 0.35);
+      });
+    });
+  }));
+
+  it("determines right camera position when marker is seen from far", testPromise(() => {
+    return makeMarkerDetector(cameraParamUrl, markerDefinitions).then((detectMarkers) => {
+      return loadImage('marker_3x3_seen_from_far.jpg').then((image) => {
+        let markers = detectMarkers(image);
+        expect(markers.length).to.equal(1);
+        expect(markers[0].id).to.equal(2);
+
+        var cameraPos = determineCameraPosition(markers[0].cameraTransform);
+        expect(cameraPos.x).to.be.within(0.40, 0.50);
+        expect(cameraPos.y).to.be.within(0.05, 0.15);
+        expect(cameraPos.z).to.be.within(0.90, 1.00);
+      });
+    });
+  }));
+
+  it("determines right camera position when marker is seen from near", testPromise(() => {
+    return makeMarkerDetector(cameraParamUrl, markerDefinitions).then((detectMarkers) => {
+      return loadImage('marker_3x3_seen_from_near.jpg').then((image) => {
+        let markers = detectMarkers(image);
+        expect(markers.length).to.equal(1);
+        expect(markers[0].id).to.equal(2);
+
+        var cameraPos = determineCameraPosition(markers[0].cameraTransform);
+        expect(cameraPos.x).to.be.within(0.05, 0.15);
+        expect(cameraPos.y).to.be.within(-0.05, 0.05);
+        expect(cameraPos.z).to.be.within(0.15, 0.25);
       });
     });
   }));
