f0eea0301d2c2bb978806ddfd0dd4437c8aa7aab
Lisa Junger <ljunger@thoughtworks.com>
2016-10-05 13:26:13 +0200

    remove trailing whitespace

--- spec/browser/convoUiSpec.js
+++ spec/browser/convoUiSpec.js
@@ -25,8 +25,5 @@ describe("welcome page", () => {
 
     chai.expect(document.querySelectorAll('ul li')).to.have.text('Hi, my name is Robb.Are you looking for a product? I can show you the way to any product in this store.I will ask your smartphone for permission to activate the camera, so you can see me. Are you okay with that?');
     chai.expect(document.querySelector('ul.chat-list')).to.have.length(4);
-
   });
-
-
 });
