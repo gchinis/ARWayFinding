6c9c59c5c240b5e0f4a46b8a871beaaf819a6a95
Martin Soto <donsoto@gmail.com>
2016-10-05 12:34:21 +0200

    Add eyes to the robot.

--- app/3dsandbox.js
+++ app/3dsandbox.js
@@ -106,12 +106,16 @@ const makeRoom = () => {
 };
 
 const makeRobot = () => {
+  const robotColor = 0xe54230;
+  const robotRadius = 0.25;
+  const bodyHeight = 0.4;
+
   var robot = new THREE.Object3D();
 
   var robotBody = new THREE.Mesh(
-    new THREE.CylinderGeometry(0.25, 0.25, 0.4, 15, 10),
+    new THREE.CylinderGeometry(robotRadius, robotRadius, bodyHeight, 15, 10),
     new THREE.MeshLambertMaterial({
-      color: 0xcf2828,
+      color: robotColor,
       wireframe: false
     })
   );
@@ -119,14 +123,40 @@ const makeRobot = () => {
   robot.add(robotBody);
 
   var robotHead = new THREE.Mesh(
-    new THREE.SphereGeometry(0.25, 15),
+    new THREE.SphereGeometry(robotRadius, 15),
     new THREE.MeshLambertMaterial({
-      color: 0xcf2828,
+      color: robotColor,
       wireframe: false
     })
   );
-  robotHead.position.y = 0.4;
+  robotHead.position.y = bodyHeight;
   robot.add(robotHead);
+
+  var robotEye1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.05, 5),
+    new THREE.MeshLambertMaterial({
+      color: 0x000000,
+      wireframe: false
+    })
+  );
+  robotEye1.translateY(bodyHeight);
+  robotEye1.rotateY(-Math.PI / 6);
+  robotEye1.rotateZ(Math.PI / 8);
+  robotEye1.translateX(robotRadius);
+  robot.add(robotEye1);
+
+  var robotEye2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.05, 5),
+    new THREE.MeshLambertMaterial({
+      color: 0x000000,
+      wireframe: false
+    })
+  );
+  robotEye2.translateY(bodyHeight);
+  robotEye2.rotateY(Math.PI / 6);
+  robotEye2.rotateZ(Math.PI / 8);
+  robotEye2.translateX(robotRadius);
+  robot.add(robotEye2);
 
   return robot;
 };
